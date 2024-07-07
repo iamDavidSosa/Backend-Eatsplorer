e7a5e2749d3bd32dc354f22208897a3565da6174
iamDavidSosa <a.davidsosa.t@gmail.com>
2024-07-06 20:05:48 -0400

    Creacion de clase Utilizades y sus metodos para encriptar con Sha256 y para generar JWT

---
+++ Custom/Utilidades.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Security.Cryptography;
+using PROYECTO_PRUEBA.Models;
+
+namespace PROYECTO_PRUEBA.Custom
+{
+    public class Utilidades
+    {
+        private readonly IConfiguration _configuration;
+
+        public Utilidades(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public string EncriptarSHA256(string cadena)
+        {
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(cadena));
+
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+
+                return builder.ToString();
+            }
+        }
+
+        public string GenerarToken(Usuario usuario)
+        {
+            var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            var credentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+
+            var userClaims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.NameId, usuario.id_usuario.ToString()),
+                new Claim(JwtRegisteredClaimNames.Email, usuario.correo!)
+            };
+
+            var tokenOptions = new JwtSecurityToken(
+                claims: userClaims,
+                expires: DateTime.Now.AddMinutes(10),
+                signingCredentials: credentials
+            );                                                                                          );
+
+            var tokenString = new JwtSecurityTokenHandler().WriteToken(tokenOptions);
+
+            return tokenString;
+        }
+    }
+}
