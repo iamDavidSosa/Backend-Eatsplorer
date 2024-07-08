3c95d9a02cbd827de225b84527c58e4afa105a33
Ardelis10 <ardelis.ulloa@gmail.com>
2024-07-07 18:53:12 -0600

    Creacion de clase Recetas

---
+++ Controllers/RecetasController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PROYECTO_PRUEBA.Custom;
+using PROYECTO_PRUEBA.Models;
+using PROYECTO_PRUEBA.Models.DTOs;
+using Microsoft.AspNetCore.Authorization;
+using PROYECTO_PRUEBA.Context;
+using Microsoft.EntityFrameworkCore.SqlServer.Query.Internal;
+
+namespace PROYECTO_PRUEBA.Controllers
+{
+    [Route("api/[controller]")]
+    [Authorize]
+    [ApiController]
+    public class RecetasController : ControllerBase
+    {
+        /*  private readonly AppDbContext _appDbContext;
+
+          public RecetasController(AppDbContext appDbContext)
+          {
+              _appDbContext = appDbContext;
+          }
+
+
+          [HttpGet]
+          [Route("Lista")]
+
+
+          public async Task<IActionResult> Lista()
+          {
+              var lista = await _appDbContext.Recetas.ToListAsync();
+              return StatusCode(StatusCodes.Status200OK, new{value = lista});
+
+           }*/
+
+
+
+    }
+}
---
+++ Models/Recetas.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.Blazor;
+using System.ComponentModel.DataAnnotations;
+
+namespace PROYECTO_PRUEBA.Models
+{
+    public class Recetas
+    {
+        [Key]    
+        public int id_receta { get; set; }
+        public string titulo { get; set; }
+        public string descripcion { get; set; }
+        public string instrucciones { get; set; }
+        public string foto_receta { get; set; }
+        public int usuario_id { get; set; }
+        public DateTime fecha_creacion { get; set; }
+
+
+    }
+}
