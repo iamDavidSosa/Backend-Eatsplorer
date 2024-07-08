d3925dc23e21d64983445f38d0d9cf8ba7566ddb
Ardelis10 <ardelis.ulloa@gmail.com>
2024-07-07 21:40:18 -0600

    Modificaciones en RecetasController

--- Controllers/RecetasController.cs
+++ Controllers/RecetasController.cs
@@ -15,26 +15,60 @@ namespace PROYECTO_PRUEBA.Controllers
     [ApiController]
     public class RecetasController : ControllerBase
     {
-          private readonly AppDbContext _appDbContext;
+        /* private readonly AppDbContext _appDbContext;
 
-          public RecetasController(AppDbContext appDbContext)
-          {
-              _appDbContext = appDbContext;
-          }
-
-
-          [HttpGet]
-          [Route("Lista")]
+         public RecetasController(AppDbContext appDbContext)
+         {
+             _appDbContext = appDbContext;
+         }
 
 
-          public async Task<IActionResult> Lista()
-          {
-              var lista = await _appDbContext.Recetas.ToListAsync();
-              return StatusCode(StatusCodes.Status200OK, new{value = lista});
-
-           }
+         [HttpGet]
+         [Route("Lista")]
 
 
+         public async Task<IActionResult> Lista()
+         {
+             var lista = await _appDbContext.Recetas.ToListAsync();
+             return StatusCode(StatusCodes.Status200OK, new{value = lista});
+
+          }*/
+
+        private readonly AppDbContext _appDbContext;
+
+        public RecetasController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        [HttpGet]
+        [Route("Lista")]
+        public async Task<IActionResult> Lista()
+        {
+            var lista = await _appDbContext.Recetas     //Usa el contexto de la base de datos (_appDbContext) para obtener una lista de todas las recetas
+                .Include(r => r.Recetas_Ingredientes)   //Incluye la colección Recetas_Ingredientes relacionada con cada receta
+                    .ThenInclude(ri => ri.Ingrediente)  //Dentro de cada Recetas_Ingredientes, incluye la entidad Ingrediente relacionada
+                .ToListAsync();                         //Convierte el resultado en una lista de forma asincrónica
+
+
+            //Transforma la lista de recetas en una nueva lista de objetos anónimos con las propiedades deseadas
+            //Incluye las propiedades de la receta y una lista de nombres de ingredientes asociados
+
+            var result = lista.Select(r => new          
+            {
+                r.id_receta,
+                r.titulo,
+                r.descripcion,
+                r.instrucciones,
+                r.foto_receta,
+                r.usuario_id,
+                r.fecha_creacion,
+                Ingredientes = r.Recetas_Ingredientes.Select(ri => ri.Ingrediente.nombre).ToList()
+            }).ToList();
+
+            //Devuelve un resultado de estado HTTP 200 (OK) 
+            return StatusCode(StatusCodes.Status200OK, new { value = result });
+        }
 
     }
 }
