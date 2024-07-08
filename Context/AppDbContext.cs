694733f26655af5a1d6c69abfd3df001d40206d8
Ardelis10 <ardelis.ulloa@gmail.com>
2024-07-07 21:25:48 -0600

    Modificacion en AppDbContext

--- Context/AppDbContext.cs
+++ Context/AppDbContext.cs
@@ -18,5 +18,26 @@ namespace PROYECTO_PRUEBA.Context
         public DbSet<Recetas> Recetas { get; set; }
 
         public DbSet<Ingredientes> Ingredientes { get; set; }
+
+        public DbSet<Recetas_Ingredientes> Recetas_Ingredientes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Configura la clave primaria compuesta para la tabla intermedia Recetas_Ingredientes
+            modelBuilder.Entity<Recetas_Ingredientes>()
+                .HasKey(ri => new { ri.id_receta, ri.id_ingrediente });
+
+            // Configura la relación muchos a muchos entre Receta y Ingrediente a través de Recetas_Ingredientes
+           /* modelBuilder.Entity<Recetas_Ingredientes>()
+                .HasOne(ri => ri.Receta)
+                .WithMany(r => r.Recetas_Ingredientes)
+                .HasForeignKey(ri => ri.RecetaId);
+
+
+            modelBuilder.Entity<Recetas_Ingredientes>()
+                .HasOne(ri => ri.Ingrediente)
+                .WithMany(i => i.Recetas_Ingredientes)
+                .HasForeignKey(ri => ri.IngredienteId);*/
+        }
     }
 }
