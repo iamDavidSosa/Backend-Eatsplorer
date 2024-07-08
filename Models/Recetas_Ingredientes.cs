056f0dafbf66d9d6762c6019787aaf8866137839
Ardelis10 <ardelis.ulloa@gmail.com>
2024-07-07 21:10:24 -0600

    Se agrego clase Recetas_Ingredientes

--- Models/Recetas_Ingredientes.cs
+++ Models/Recetas_Ingredientes.cs
@@ -1,6 +1,10 @@
-﻿namespace PROYECTO_PRUEBA.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PROYECTO_PRUEBA.Models
 {
     public class Recetas_Ingredientes
     {
+        public int id_receta { get; set; }
+        public string id_ingrediente { get; set; }
     }
 }
