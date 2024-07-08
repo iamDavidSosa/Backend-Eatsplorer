558bc066d0aa45db26941fef55033809575b8102
Ardelis10 <ardelis.ulloa@gmail.com>
2024-07-07 22:33:45 -0600

    Modificacion en Recetas_Ingredientes

--- Models/Recetas_Ingredientes.cs
+++ Models/Recetas_Ingredientes.cs
@@ -7,7 +7,7 @@ namespace PROYECTO_PRUEBA.Models
         public int id_receta { get; set; }
 
         public Recetas Recetas { get; set; }
-        public string id_ingrediente { get; set; }
+        public int id_ingrediente { get; set; }
 
         public Ingredientes Ingrediente { get; set; }
     }
