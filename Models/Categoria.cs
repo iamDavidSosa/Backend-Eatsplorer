ba24ba511f6e2b7cd6a661907a34935fd69dc977
Ardelis10 <ardelis.ulloa@gmail.com>
2024-07-30 21:51:06 -0600

    Creacion de clase Categoria

---
+++ Models/Categoria.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PROYECTO_PRUEBA.Models
+{
+    public class Categoria
+    {
+        [Key]
+        public int id_categoria { get; set; }
+        public int nombre { get; set; }
+    }
+}
