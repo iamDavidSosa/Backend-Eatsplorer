890bae624fc35d02aaeaa56ea73f908092ff9d98
Ardelis10 <ardelis.ulloa@gmail.com>
2024-07-30 21:48:21 -0600

    Creacion de clase Tags

---
+++ Models/Tags.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PROYECTO_PRUEBA.Models
+{
+    public class Tags
+    {
+        [Key]
+        public int id_tag { get; set; }
+        public int nombre { get; set; }
+    }
+}
