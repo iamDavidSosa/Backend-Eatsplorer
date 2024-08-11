d62d21b1329da984e27b53db3d3f97bda94c4190
Ardelis10 <ardelis.ulloa@gmail.com>
2024-08-10 20:40:52 -0600

    Cambios en Ingredientes y eliminacion de clase y controlador Medida

--- Context/AppDbContext.cs
+++ Context/AppDbContext.cs
@@ -24,11 +24,9 @@ namespace PROYECTO_PRUEBA.Context
 
         public DbSet<Recetas_Guardadas> Recetas_Guardadas { get; set; }
 
-        public DbSet<Medida> Medida { get; set; }
-
         public DbSet<Detalles_Usuario> Detalles_Usuario { get; set; }
 
-        // public DbSet<Despensa> Despensa { get; set; }
+        //public DbSet<Despensa> Despensa { get; set; }
 
         public DbSet<Dias> Dias { get; set; }
 
